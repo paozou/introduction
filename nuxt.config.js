08ff8f2d5480259c981b2e2070c46a73df2783cb
paozou <sos.j.goodit@gmail.com>
2020-12-26 18:39:46 +0900

    add_metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'introduction-nuxt',
+    titleTemplate: '%s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,16 +1,76 @@
 <template>
-  <div>
-    <p>/users/_id.vue</p>
-    UserId: {{ userId }}
-  </div>
+  <section class="container">
+    <div>
+      <h3>{{ user.id }}</h3>
+      <img :src="user.profile_image_url" width="120" alt="" />
+      <p>{{ user.description || 'No description' }}</p>
+      <p>
+        <nuxt-link to="/">
+          <small>
+            <b>トップへ戻る</b>
+          </small>
+        </nuxt-link>
+      </p>
+      <h3>{{ user.id }}さんの投稿一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{ item.title }}</span>
+          </h4>
+          <div>{{ item.body.slice(0, 130) }}・・・・・・・</div>
+          <p>
+            <a target="_blank" :href="item.url">{{ item.url }}</a>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  data() {
+  async asyncData({ route, app }) {
+    const user = await app.$axios.$get(
+      `https://qiita.com/api/v2/users/${route.params.id}`
+    )
+    const items = await app.$axios.$get(
+      `https://qiita.com/api/v2/items?query=user:${route.params.id}`
+    )
+    return { user, items }
+  },
+  head() {
     return {
-      userId: this.$route.params.id,
+      title: this.user.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'userごとのdescription',
+        },
+      ],
+      link: [{ rel: 'canonical', href: '' }],
     }
   },
 }
 </script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
